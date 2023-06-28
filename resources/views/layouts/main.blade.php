b21c6a7a4a943400ccb374dcac02483bd6b4dbf3
Alzir <alzirfoto@gmail.com>
2023-06-28 14:49:55 -0300

    Commit 3

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -72,7 +72,7 @@
 </div>
 </main>
      <footer>
-        <p> Alzir Lima Events &copy; 2022 </p>
+        <p> Alzir Lima Events &copy; 2023 </p>
 </footer>
 <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
 <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
